d9228fa439cf043b24a2e3296581db7aa41a0e31
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 05:31:32 +0300

    Initial commit

---
+++ HarmonikOrtalama.m
@@ -0,0 +1,8 @@
+x=input('1. Sayýyý Giriniz:');
+y=input('2. Sayýyý Giriniz:');
+z=input('3. Sayýyý Giriniz:');
+t=input('4. Sayýyý Giriniz:');
+
+H=(4/(1/x+1/y+1/z+1/t));
+
+fprintf('Girilen deðerlerin harmonik Ortalamasý=%d \n',H);
---
+++ README.md
@@ -0,0 +1,2 @@
+# Matlab_8_Harmonik_Ortalama
+ 
